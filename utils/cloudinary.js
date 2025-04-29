5ccba966c23d53e2a5c661f7c05a37fc93a2afeb
omar-elnady <omar.elnadey99@gmail.com>
2025-04-29 16:45:48 +0300

    update  utils files

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,9 +1,14 @@
 import { v2 as cloudinary } from 'cloudinary';
+import dotenv from 'dotenv';
+dotenv.config();
 
+if (!process.env.CLOUDINARY_CLOUD_NAME || !process.env.CLOUDINARY_API_KEY || !process.env.CLOUDINARY_API_SECRET) {
+    throw new Error("CLOUDINARY_NAME, CLOUDINARY_API_KEY, and CLOUDINARY_API_SECRET are required but not found in env variable");
+}
 cloudinary.config({
-    cloud_name: 'dm1ziouxl',
-    api_key: '651333414773345',
-    api_secret: "PdmpjDAxSEyhP4NTpoReRI2cPxE",
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
     secure: true
 });
 
--- utils/multer.cloudinary.js
+++ utils/multer.cloudinary.js
@@ -1,11 +1,11 @@
 import multer from 'multer'
+
 export const fileVaildation = {
     image: ['image/jpeg', 'image/png', 'image/gif'],
     files: ['application/pdf', 'application/msword']
 }
 
 export function fileUplode(vaildationFile) {
-
     const storage = multer.diskStorage({})
     function fileFilter(req, file, cb) {
         if (vaildationFile.includes(file.mimetype)) {
