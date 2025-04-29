c5c6e158364b9fb2b4d07f72fcee8f5190438655
omar-elnady <omar.elnadey99@gmail.com>
2025-04-29 16:51:11 +0300

    delete multer upload file in project form main js

--- Src/index.router.js
+++ Src/index.router.js
@@ -1,23 +1,27 @@
 import connection from '../DB/connection.js'
 import { globalHandlingError } from '../utils/errorHandling.js'
-import authRouter from './modules/auth/auth.router.js'
 import cors from 'cors'
+
+// Import Routers
+import authRouter from './modules/auth/auth.router.js'
 import blogs from './modules/blogs/blogs.router.js'
 import sendEmail from './modules/sendEmail/sendEmail.rotuer.js'
 
 
 const bootstarp = (app, express) => {
-    app.use(cors())
-    app.use(express.json({ limit: '10mb' }))
-    app.use(express.urlencoded({ extended: true }))
-    app.use(express.json())
+    app.use(cors());
+    app.use(express.json({ limit: '10mb' }));
+    app.use(express.urlencoded({ extended: true }));
+
+    connection()
+
     app.use('/api/auth', authRouter)
     app.use('/api/blogs', blogs)
     app.use('/api/sendemail', sendEmail)
+    
     app.get("*", (req, res, next) => {
         res.json({ message: "invalid routing " })
     })
-    connection()
     app.use(globalHandlingError)
 }
 
--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 import express from "express";
 import bootstarp from "./Src/index.router.js";
 const app = express()
-const port = process.env.port || 5000
-import path from "path";
-import { fileURLToPath } from "url";
-const __dirname = fileURLToPath(import.meta.url)
-app.use("/uploads", express.static(path.join(__dirname, "./src/uploads")))
+const port = process.env.PORT || 5000
+
 bootstarp(app, express)
 app.listen(port, () => {
     console.log('app is running in port ' + port)
