c990fe046cf189b47bd8d7ac8432b7ca5f80f44e
omar-elnady <omar.elnadey99@gmail.com>
2025-04-29 17:37:52 +0300

    import env in main js and edit code of auth js

--- Src/modules/auth/controller/auth.js
+++ Src/modules/auth/controller/auth.js
@@ -1,44 +1,45 @@
-import userModel from "../../../../DB/Models/user.model.js"
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
+import userModel from "../../../../DB/Models/user.model.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
-import { asyncHandler } from "../../../../utils/errorHandling.js"
+import { asyncHandler } from "../../../../utils/errorHandling.js";
 
-const secretJWT = 'iwan-website-project-nodejs'
+export const register = asyncHandler(async (req, res, next) => {
+  const { userName, password, cPassword } = req.body;
+  const isExist = await userModel.findOne({ userName });
+  if (password != cPassword) {
+    return next(new Error("Confirm Password not match password"));
+  }
+  if (isExist) {
+    return next(new Error("userName is aleady exist"));
+  }
+  const hashPassword = bcrypt.hashSync(
+    password,
+    Number(process.env.SALT_ROUNDS)
+  );
+  const user = await userModel.create({
+    userName,
+    password: hashPassword,
+  });
+  return res.json({
+    message: "Done",
+    user: { userName: user.userName, _id: user._id },
+  });
+});
 
-export const register = asyncHandler(
-    async (req, res, next) => {
-        const { userName,
-            password,
-            cPassword,
-             } = req.body
-        const isExist = await userModel.findOne({ userName })
-        if (password != cPassword) {
-            return next(new Error('cPassword not match password'))
-        }
-        if (isExist) {
-            return next(new Error('userName is aleady exist'))
-        }
-        const hashPassword = bcrypt.hashSync(password, 8);
-        const user = await userModel.create({
-            userName,
-            password: hashPassword,
-        })
-        return res.json({ message: 'Done' })
-    })
-
-export const login = asyncHandler(
-    async (req, res, next) => {
-        const { userName, password } = req.body
-        const checkUser = await userModel.findOne({ userName })
-        if (!checkUser) {
-            return next(new Error("userName is not exist"))
-        }
-        const comparePassword = bcrypt.compareSync(password, checkUser.password)
-        if (!comparePassword) {
-            return next(new Error('userName or password not correct'))
-        }
-        const token = jwt.sign({  userName, id: checkUser._id }, secretJWT);
-        res.json({ message: 'Done', token })
-    }
-)
+export const login = asyncHandler(async (req, res, next) => {
+  const { userName, password } = req.body;
+  const checkUser = await userModel.findOne({ userName });
+  if (!checkUser) {
+    return next(new Error("Username is not exist"));
+  }
+  const comparePassword = bcrypt.compareSync(password, checkUser.password);
+  if (!comparePassword) {
+    return next(new Error("Username or Password not correct"));
+  }
+  const token = jwt.sign(
+    { userName, id: checkUser._id },
+    process.env.SECRETJWT_KEY
+  );
+  res.json({ message: "Done", token });
+});
--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import express from "express";
 import bootstarp from "./Src/index.router.js";
+import dotenv from "dotenv";
+
 const app = express()
 const port = process.env.PORT || 5000
+  dotenv.config();
 
 bootstarp(app, express)
 app.listen(port, () => {
